ca6342accfd8ccb6afaa923539c1c211746d5337
Mathias <mathfernandes@usp.br>
2020-12-04 22:05:07 -0300

    Population: population applied mutation from one to next generation, no crossover

--- src/main.cpp
+++ src/main.cpp
@@ -17,16 +17,14 @@ using namespace std;
         popmax: max population
         elite:
 */
-Population createPopulation(const vector<vector<double>>& matrix, double mutation_rate, int popmax, int elite) {
-    return Population(matrix, mutation_rate, popmax, elite);
+Population createPopulation(const vector<vector<double>>& matrix, int popmax, int elite) {
+    return Population(matrix, popmax, elite);
 }
 
 /*
 */
 void run(Population population) {
-    int i = 0, max_gen = 200;
-
-    population.start_clock();
+    int i = 0, max_gen = 2;
 
     while(i++ < max_gen) {
         cout << "Generation: " << population.generation << endl;
@@ -36,22 +34,19 @@ void run(Population population) {
         population.calc_fitness();
         cout << "Evaluate the best crew\n";
 
-        // Get best individual
-        population.evaluete();
-        cout << "Best fitniss: " << population.best_individual.fitness << endl;
-
         // Select actual population
         population.selection();
-        cout << "Population selected\n";
+        cout << "Best fitniss: " << population.best_individual.fitness << endl;
+        cout << "Length: " << population.best_individual.distance << endl;
 
         // Reproduction of new life, called also sex
         population.generate();
         cout << "Population sucessiful crossovered\n";
         
-        if(population.get_is_finish()) {
-            cout << "End of Evolution" << endl;
-            return;
-        }
+        // if(population.get_is_finish()) {
+        //     cout << "End of Evolution" << endl;
+        //     return;
+        // }
     }   
 }
 
@@ -75,20 +70,20 @@ vector<vector<double>> load_distance_matrix() {
 
 // will be deleted 
 int main() {
-    string target = "TO BE OR NOT TO BE";
-    int popmax = 2500;
-    double mutationRate = 0.02;
+    vector<vector<double>> matrix = load_distance_matrix();
+    int popmax = 7;
     int elite = 5;
     
-    vector<vector<double>> matrix = load_distance_matrix();
-
-    for(int i = 0; i < 10; ++i)
-        for(int j = 0; j < 10; ++j)
+    for(int i = 0; i < 10; ++i) {
+        for(int j = 0; j < 10; ++j) {
             cout << matrix[i][j] << ' ';
+
+        }
         cout << endl;
+    }
 
     // Create population initial
-    Population population = createPopulation(matrix, mutationRate, popmax, elite);
+    Population population = createPopulation(matrix, popmax, elite);
     
     // Run life
     run(population);
--- src/population.h
+++ src/population.h
@@ -2,39 +2,53 @@
 #define POPULATION_H
 
 #include<string>
-#include<vector>
+#include<vector>   
+#include <algorithm> // swap
 #include "DNA.h"
 
 using namespace std;
 
 const double PERFECT_SCORE = 1;
 
+bool sort_individuals(const DNA& ind_a, const DNA& ind_b) {
+    return (ind_a.fitness > ind_b.fitness);
+}
+
+void suffle(vector<double>& tour, int num) {
+    int length = (int)tour.size();
+    for(int i = 0; i < num; ++i) {
+        int n1 = rand() % length,
+            n2 = rand() % length;
+        swap(tour[n1], tour[n2]);
+    }
+}
+
 /*
     Population is a collection of elements
     Element or DNA is a collection of genes
 
-    Target -> optimal solution for the element
+    distance_matrix -> optimal solution for the element
 
 */
 class Population {
     public:
-        vector<vector<double>> target;             
-        double mutation_rate;
+        vector<vector<double>> distance_matrix;             
+        double fitness_avg, fitness_max;
         int popmax, generation, elite;
         bool is_finish;
         DNA best_individual;        // best individual at moment
         vector<DNA> individuals;    // actual generation
         vector<DNA> mating_pool;
 
-        Population(const vector<vector<double>>& target, double mutation, int popmax, int elite) {
-                this->target = target;
-                this->mutation_rate = mutation;
+        Population(const vector<vector<double>>& distance_matrix, int popmax, int elite) {
+                this->start_clock();
+                this->distance_matrix = distance_matrix;
                 this->popmax = popmax;
                 this->elite = elite;
                 generation = 0;
                 is_finish = false;
                 for(int i = 0; i < popmax; ++i) {
-                    individuals.push_back(DNA((int)target.size()));
+                    individuals.push_back(DNA((int)distance_matrix.size()));
                 }
         }
 
@@ -42,64 +56,95 @@ class Population {
         void start_clock() {
             srand (time(NULL));
         }
-        // Select individuals with more fitness most likely to survive
-        // Select the BESTEST individuals based on fitness
-        void selection() {
 
-            double max_fitness = 0.0;
-            for(int i = 0; i < popmax; ++i) {
-                max_fitness = max(max_fitness, this->individuals[i].fitness);
-            }
-            max_fitness *= 100;
-            for(int i = 0; i < popmax; ++i) {
-                int n = 1;
-                if(max_fitness > 0)
-                    n = ((double)(100*this->individuals[i].fitness) / (double) max_fitness);
-                for(int j = 0; j < n; ++j)
-                    mating_pool.push_back(DNA(this->individuals[i]));
+        /* Print fitness */
+        void print_fitness() {
+            cout << "Fits: ";
+            for(int i = 0; i < popmax; ++i)
+                cout << individuals[i].fitness << ' ';
+            cout << endl;
+        }
+
+        /*
+            Battle of bank corruping and luck
+
+            Forces of destination that can not be changed.  
+        */
+        void tournament_of_two() {
+            for(int i = 0; i < 2; ++i) {
+                int falling = (int)(rand() % this->elite);
+                int rising =  (int)(rand() % (this->popmax-this->elite)) + this->elite-2;
+                swap(this->individuals[falling], this->individuals[rising]);
             }
         }
 
-        //
+        /* 
+            Select individuals with more fitness most likely to survive
+            Save the best invidividual
+        */
+        void selection() {
+
+            double sum_fitness = 0.0;            
+
+            // Sum all fitness 
+            for(int i = 0; i < popmax; ++i) {
+                for(int j = 0; j < this->individuals[i].length; ++j) {
+                    cout << this->individuals[i].genes[j] << ' ';
+                }
+                cout << endl;              
+                sum_fitness += this->individuals[i].fitness;
+            }
+            // Sort individuals by decrement fitness 
+            sort(individuals.begin(), individuals.end(), sort_individuals);
+            
+            // Save fitness average and max of this population
+            this->fitness_avg = sum_fitness / popmax;
+            this->fitness_max = individuals[0].fitness;
+
+            print_fitness();
+
+            // Check if there is a new best individual
+            if(individuals[0].fitness > this->best_individual.fitness)
+                this->best_individual = DNA(individuals[0]);
+
+            // Change two of elite for two individual with poor fitness
+            tournament_of_two();
+        }
+
+        /*
+        
+        */
         void generate() {
             // cout << "Start generate\n";
-            int num_individuals = (int)this->mating_pool.size();
             for(int i = 0; i < popmax; ++i) {
                 // cout << "Start crossover\n";
-                int father = rand() % num_individuals;
-                int mother = rand() % num_individuals;
-                DNA child = DNA(this->mating_pool[father].crossover(this->mating_pool[mother]));
+                int elite_index = (int)(rand() % elite);
+                // DNA child = DNA(this->individuals[i].crossover(this->individuals[elite_index]));
                 // cout << "CHILD FIT: " << child.fitness << endl; 
-                child.mutate(this->mutation_rate);
-                this->individuals[i] = child;
+                // child.mutate(this->fitness_avg, this->fitness_max);
+                individuals[i].mutate(this->fitness_avg, this->fitness_max);
+                // this->individuals[i] = child;
                 // cout << "chil " << child.genes << endl;
             }
             // cout << "End generate\n";
             // Next generation is made
             this->generation++;
-            mating_pool.clear();
         }
 
         // Calculate fitness for every individual
         void calc_fitness() {  
-            // cout << "Eszn\n";
-            for(int i = 0; i < popmax; ++i) {
-                individuals[i].calc_fitness(target);
-            } 
-        }
+            double sum = 0.0;
 
-        void evaluete() {
-            double max_fit = -1.0;
-            int j = 0;
+            // Evaluate fitness as fit = 1 / d
             for(int i = 0; i < popmax; ++i) {
-                // cout << individuals[i].fitness << endl;
-                if(individuals[i].fitness > max_fit) {
-                    max_fit = individuals[i].fitness;
-                    j = i;
-                }
-            }
-            this->best_individual = DNA(individuals[j]);
-            cout << "Best fitness: " << best_individual.genes << ' ' << best_individual.fitness << '\n';
+                individuals[i].calc_fitness(distance_matrix);
+                sum += individuals[i].fitness;
+            } 
+
+            // Normalize with fit/total fit 
+            for(int i = 0; i < popmax; ++i) {
+                individuals[i].fitness /= sum;
+            } 
         }
 
         bool get_is_finish() {
